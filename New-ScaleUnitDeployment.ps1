fd59861883c0966b386d28cc2330a4cd89f85bb2
Nadeem Nasir <nadeem.nasir@myindicators.se>
2020-03-31 20:39:45 +0200

    scale unit file

--- New-ScaleUnitDeployment.ps1
+++ New-ScaleUnitDeployment.ps1
@@ -7,4 +7,4 @@
 #  dit: development integration test environment
 
 New-AzResourceGroupDeployment -Name 'deployment' -ResourceGroupName 'sof-case-we-dit' -Mode Incremental -TemplateFile 'sof-exchangerate.json' -TemplateParameterFile 'sof-exchangerate-dit-parameters.json'
-az group deployment  create --resource-group  'sof-case-we-dit'  --template-file ./sof-exchangerate.json --parameters ./sof-exchangerate-dit-parameters.json
+#az group deployment  create --resource-group  'sof-case-we-dit'  --template-file ./sof-exchangerate.json --parameters ./sof-exchangerate-dit-parameters.json
