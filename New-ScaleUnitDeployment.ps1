d55bc9f4aa8ad6403b676967af14b2d75b22b16e
Nadeem Nasir <nadeem.nasir@myindicators.se>
2020-03-31 20:22:56 +0200

    the first commit

---
+++ New-ScaleUnitDeployment.ps1
@@ -0,0 +1,10 @@
+# Deploying scale unit case and exchange rate with bundled components
+# 
+# sof-case-we-dit
+#  sof: software
+#  case: scale unit Name
+#  we: region west europe
+#  dit: development integration test environment
+
+New-AzResourceGroupDeployment -Name 'deployment' -ResourceGroupName 'sof-case-we-dit' -Mode Incremental -TemplateFile 'sof-exchangerate.json' -TemplateParameterFile 'sof-exchangerate-dit-parameters.json'
+az group deployment  create --resource-group  'sof-case-we-dit'  --template-file ./sof-exchangerate.json --parameters ./sof-exchangerate-dit-parameters.json
